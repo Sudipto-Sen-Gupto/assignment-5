e74327e1cb87f7cd24dc5c0e68bc4015a27e2624
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-28 17:55:57 +0600

    new section

--- dynamic.js
+++ dynamic.js
@@ -3,7 +3,8 @@
 // #hundred
 //#copy
 //.love
-
+ 
+//love button
 const loveSign=document.getElementsByClassName("love");
 
 
@@ -13,5 +14,46 @@ for(const item of loveSign){
         let bigZero=parseInt(zero.innerText);
         zero.innerText=bigZero+1;
     })
-
 }
+
+//call section
+const call=document.getElementsByClassName("calling");
+for(const item of call){
+
+item.addEventListener("click",(e)=>{
+      const coin=document.getElementById("hundred")
+
+     
+ 
+    const copCoin=parseInt(coin.innerText);
+    const cutCoin=20;
+    const finalCoin=copCoin-cutCoin;
+    coin.innerText=finalCoin;
+    
+      const divi=document.getElementById("divi");
+
+      if(coin.innerText<0){
+        alert("Your number is invalid");
+        coin.innerText=0;
+        return;
+    } 
+      const childDiv=document.createElement("div");
+     childDiv.classList.add("flex","justify-between","mt-5","text-2xl","items-center","bg-gray-50","p-4","rounded-3xl")
+     
+     const first=document.getElementById("head1").innerText;
+     const second=document.getElementById("head2").innerText;
+     const date=new Date();    
+     const hour=date.getHours();
+     const minute=date.getMinutes();
+      childDiv.innerHTML=`<p>${first} </br> ${second}</p><p>${hour}:${minute}</p>`;
+      divi.append(childDiv); 
+     
+      
+    })
+}
+
+const clear=document.getElementById("clear");
+      clear.addEventListener("click",(e)=>{
+        const divi=document.getElementById("divi");
+        divi.innerHTML=""
+      })
